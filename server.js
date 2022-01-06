373493c08abcf3e4f8bf23e7105cc78a3120da37
GaneshXDev <iamganeshkumar.official@gmail.com>
2022-01-06 18:35:35 +0530

    server file changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("./dist/ganesh-portfolio-site"));
+app.use(express.static("./client/dist/ganesh-portfolio-site"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/ganesh-portfolio-site/" });
+  res.sendFile("index.html", { root: ".client/dist/ganesh-portfolio-site/" });
 });
 
 app.listen(process.env.PORT || 8080);
